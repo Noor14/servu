74c2c27acaa8b8f2087c50de0c13f8b2b13ab746
Mobin <mbutt@panteragroup.org>
2017-09-19 20:01:08 +0500

    changes

--- app/scripts/controllers/joblist.js
+++ app/scripts/controllers/joblist.js
@@ -12,6 +12,46 @@ angular.module('servu')
     function ($rootScope, jobListService, ngDialog, $state) {
 
     var vm = this;
+      vm.status = [
+        {
+          id:0,
+          name:"Rejected by Admin"
+        },
+        {
+          id:1,
+          name:"Open"
+        },
+        {
+          id:2,
+          name:"Awarded"
+        },
+        {
+          id:3,
+          name:"Started"
+        },
+        {
+          id:4,
+          name:"Completed"
+        },
+        {
+          id:5,
+          name:"In Dispute"
+        },
+        {
+          id:6,
+          name:"Signed Off"
+        }
+
+      ];
+
+      vm.getStatus = function(id){
+        return vm.status.find(function(obj){
+            if (obj.id === id){
+              return obj.name;
+            }
+        })
+      };
+
     vm.accountInfo = JSON.parse(localStorage.getItem("userDetail"));
     vm.userData = vm.accountInfo.data.user;
     vm.current_time;
@@ -30,6 +70,7 @@ angular.module('servu')
         vm.page_no =  res.data.page;
         vm.total_pages = res.data.total_pages;
         vm.records = res.data.total_records;
+        vm.jobTime = res.data.timestamp;
         vm.jobs = res.data.jobs;
       });
     };
